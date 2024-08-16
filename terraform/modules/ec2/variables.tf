1580ab55b66ab89612726f55ff4e377b431917cf
William Barros <81761814+fwilliambarros@users.noreply.github.com>
2024-08-16 17:14:28 -0300

    Update variables.tf

--- terraform/modules/ec2/variables.tf
+++ terraform/modules/ec2/variables.tf
@@ -8,11 +8,6 @@ variable "instance_type" {
   default     = "t2.micro"
 }
 
-# variable "key_name" {
-#   description = "The name of the key pair to use for the instance"
-#   type        = string
-# }
-
 variable "name" {
   description = "The name tag for the EC2 instance"
   type        = string
